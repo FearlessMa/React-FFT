010e4352a72a86dcd2d58cbd22efde9178bcfb86
mhc <mahaichao1030@qq.com>
2018-05-09 09:55:44 +0800

    修复

--- src/common/index.js
+++ src/common/index.js
@@ -8,6 +8,6 @@ import { TableComponent } from './tableComponent';
 import { tranTreeData } from './tranTreeData';
 import { axiosPost, requestData, alertNotification, alertModal } from './axios';
 import { BreadcrumbComponent } from './breadcrumbComponent'
-
+import './index.less';
 export { FormComponent, TableComponent, tranTreeData, tranCheckboxData, axiosPost, requestData, alertNotification, 
     alertModal, BreadcrumbComponent }
